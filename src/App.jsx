81625c3b6d7f59d715a7c9e663f8ce462eba7f61
GregGTest <greggdev23@gmail.com>
2021-03-23 18:58:37 +0000

    Creating template

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,10 @@ const App = () => {
                 <img src={basketIcon} alt='Basket icon' width='30px'/>
             </nav>
             <Product/>
+            <footer>
+                <p>Octopus Energy Ltd is a company registered in England and Wales.</p>
+                <p>Registered number: 09263424. Registered office: 33 Holborn, London, EC1N 2HT. Trading office: 20-24 Broadwick Street, London, W1F8HT</p>
+            </footer>
         </div>
     )
 };
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -8,8 +8,65 @@ function Product() {
 
     if (loading) return <div>Loading...</div>;
     if (error) return <div>`Error! ${error.message}`</div>;
+
+    const priceInPounds = (data.product.price/100).toFixed(2);
+    const splitPrice = priceInPounds.split('.');
+    const pounds = splitPrice[0];
+    const pence = splitPrice[1];
+    
     return(
         <div className='product'>
+            <div className='imageContain'>
+                <img src={data.product.imgUrl} alt={'Image for ' + data.product.name} />
+            </div>
+                <header>
+                    <h1>{data.product.name}</h1>
+                    <p className='productInfo'>{data.product.power} {'//'} Packet of {data.product.quantity}</p>
+                </header>
+                <div className='priceContain'>
+                    <p className='price'>
+                        <span className='pounds'>{pounds}</span>
+                        <sup className='pence'>.{pence}</sup>
+                    </p>
+                    <div className='quantity'>
+                        <label>QTY</label>
+                        <div className='buttons'>
+                            <button>-</button> 1 <button>+</button>
+                        </div>
+                    </div>
+                    <button className='addToCart'>Add to cart</button>
+                </div>
+                <main className='description'>
+                    <h2>Description</h2>
+                    <div>{data.product.description}</div>
+                </main>
+                <section className='specifications'>
+                    <h2>Specifications</h2>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Brand</td>
+                                <td>{data.product.brand}</td>
+                            </tr>
+                            <tr>
+                                <td>Item weight</td>
+                                <td>{data.product.weight}</td>
+                            </tr>
+                            <tr>
+                                <td>Dimensions</td>
+                                <td>{data.product.height} x {data.product.width} x {data.product.length}</td>
+                            </tr>
+                            <tr>
+                                <td>Item model number</td>
+                                <td>{data.product.modelCode}</td>
+                            </tr>
+                            <tr>
+                                <td>Colour</td>
+                                <td>{data.product.colour}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
         </div>
     )
 }
